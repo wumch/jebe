1beb515973a63a2832267fe323ec4b7895ad1fb8
frank <wumuch@gmail.com>
2012-08-01 03:55:59 +0800

    crawler server

--- target/dealer-client.py
+++ target/dealer-client.py
@@ -3,11 +3,78 @@
 import zmq
 from natip import natip
 import zlib, struct
+import riak
+from random import randint
+from hashlib import md5
+from urllib2 import *
 from json import JSONDecoder, JSONEncoder
+from utils.UrlParser import UrlParser
 
-context = zmq.Context(io_threads=1)
+context = zmq.Context()
 sock = context.socket(zmq.REP)
 sock.connect("tcp://%s:10011" % natip)
+
+class PageStorer(object):
+
+    urlparser = UrlParser()
+    tokenizer = ('http://192.168.88.2:10086/split',
+                 'http://192.168.88.4:10086/split',
+                 'http://127.0.0.1:10086/split',)
+    riaks = ({'host':'192.168.88.1', 'port':8098},
+             {'host':'192.168.88.2', 'port':8098},
+             {'host':'192.168.88.3', 'port':8098},
+             {'host':'192.168.88.4', 'port':8098},)
+    riaks = ({'host':'127.0.0.1', 'port':8098},)
+    buck = 'loc'
+    W_VALUE = 1
+    DW_VALUE = 0
+    R_VALUE = 1
+
+    _instance = None
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+
+    def __init__(self):
+        self.riakClient = riak.RiakClient(**self.riaks[randint(0, len(self.riaks) - 1)])
+        self.bucket = self.riakClient.bucket(self.buck)
+
+    def store(self, meta, content):
+        data = self.getData(meta, content)
+        if data is None: return
+        self.bucket.new(self.genKey(meta['url']), data).store(w=self.W_VALUE, dw=self.DW_VALUE)
+
+    def getData(self, meta, content):
+        if 'url' not in meta:
+            return None
+        urlinfo = self.urlparser.parse(meta['url'])
+        if urlinfo is None:
+            return None
+        words = self.split(content)
+        if words is None:
+            return None
+        return {
+            'loc' : urlinfo,
+            'ref' : meta['ref'] if 'ref' in meta else '',
+            'words' : words,
+        }
+
+    def split(self, content):
+        for server in self.tokenizer:
+            try:
+                return urlopen(server, content, timeout=3).read()
+            except Exception: pass
+
+    def genKey(self, url):
+        m = md5()
+        m.update(url)
+        return m.hexdigest()
+
+    def exists(self, url):
+        self.bucket.get(key=self.genKey(url), r=self.R_VALUE).exists()
 
 class Handler(object):
 
@@ -40,6 +107,8 @@ class Handler(object):
 
 class HPageExists(Handler):
 
+    pageStorer = PageStorer.instance()
+
     def __init__(self):
         super(HPageExists, self).__init__()
 
@@ -49,35 +118,37 @@ class HPageExists(Handler):
             non-exists: tell client, ask for details of the page.
             exists: response "all right" with some ads.
         """
-        global sock
         try:
-            info = self.jsonDecoder.decode(data)
-        except Exception:
-            sock.send(self.ERR_CODE_ERR)
-            print "pageExists failed"
-            return
-        print info
-        sock.send(self.ERR_CODE_OK)
+            print data
+            info = self.jsonDecoder.decode(data[0])
+            print info
+            self.replyOk() if self.pageStorer.exists(info['url']) else self.replyErr()
+        except Exception, e:
+            self.replyErr()
+            print "pageExists failed: ", e.message()
 
 class HCrawl(Handler):
 
     ucpacker = struct.Struct('B')     # ny
+    pageStorer = PageStorer.instance()
 
     def __init__(self):
         super(HCrawl, self).__init__()
 
     def handle(self, data):
-        global sock
         try:
             meta = self.jsonDecoder.decode(data[0])
             content = zlib.decompress(data[1]) if meta['compressed'] else data[1]
-        except Exception:
-            sock.send(self.ERR_CODE_ERR)
-            print "crawl failed"
-            return
-        print meta
-        print content
-        sock.send(self.ERR_CODE_OK)
+            self.store(meta, content)
+            self.replyOk()
+            print meta
+            print content
+        except Exception, e:
+            self.replyErr()
+            print "crawl failed: ", e.message()
+
+    def store(self, meta, content):
+        self.pageStorer.store(meta, content)
 
 class HShowAds(Handler):
 
---
+++ target/utils/UrlParser.py
@@ -0,0 +1,68 @@
+#coding:utf-8
+
+from urlparse import urlparse
+
+class UrlParser(object):
+
+    _type = ('com', 'mobi', 'gov', 'edu', 'so', 'net', 'org', 'name', 'me', 'co', 'com', 'net', 'tel', 'info', 'biz', 'cc', 'tv')
+    _area = ('cn', 'hk', 'jp', 'ko', 'ra')
+
+    def parse(self, url):
+        return self.splitUrl(url)
+
+    def splitUrl(self, url):
+        info = urlparse(url)
+        scheme = self.parseScheme(info[0])
+        domain = self.parseDomain(info[1])
+        if scheme is None or domain is None:
+            return None
+        res = scheme + ' ' + ' '.join(domain)
+        path = self.parsePath(info[2])
+        if path:
+            res += ' ' + ' '.join(path)
+        query = self.parseQuery(info[4])
+        if query:
+            res += ' ' + ' '.join(query)
+        return res
+
+    def parsePath(self, path):
+        info = filter(len, [fragment.strip() for fragment in path.split(r'/')])
+        return [(r'/' * i + info[i]) for i in range(0, len(info))]
+
+    def parseDomain(self, domain):
+        info = (domain[:domain.find(':')] if ':' in domain else domain).split('.')
+        parts = len(info)
+        if parts == 1:
+            return None
+        elif parts == 2:
+            return info[:1]
+        else:
+            last = info[-1]
+            if last in self._type:
+                subLen = parts - 2
+            elif last in self._area:
+                subLen = parts - 3
+            else:
+                subLen = parts - 2
+            return '.'.join(info[:subLen]), '.'.join(info[subLen:])
+
+    def parseQuery(self, query):
+        return filter(len, query.split('&'))
+
+    def parseScheme(self, scheme):
+        return scheme if scheme in ('http', 'https') else None
+
+def test(url):
+    import os
+    parser = UrlParser()
+    print 'url:  ', url, os.linesep,                        \
+        'info: ', parser.parse(url), os.linesep, os.linesep
+
+if __name__ == '__main__':
+    test('http://www.baidu.com')
+    test('http:/www.baidu.com')
+    test('http://www.baidu.com?fsdf=8998&fsdaf=fsdf')
+    test('http://www.baidu.com/fasdfd/afsdfadf/fasdf?fsdf=8998&fsdaf=fsdf')
+    test('http://yundao.hi.baidu.com.cn/fasdfd/afsdfadf/fasdf?fsdf=8998&fsdaf=fsdf')
+    test('http://yundao.hi.baidu.com.xo/fasdfd/afsdfadf/fasdf?fsdf=8998&fsdaf=fsdf')
+    test('http://yundao.hi.baidu.cxx.xo/fasdfd/afsdfadf/fasdf?fsdf=8998&fsdaf=fsdf')
