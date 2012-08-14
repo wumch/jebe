66d4650cb62a60de7cdc0b964eb12e052d26c0ed
frank <wumuch@gmail.com>
2012-08-14 18:08:00 +0800

    revision

--- toolkit/iptables.sh
+++ toolkit/iptables.sh
@@ -5,10 +5,12 @@
 # make gad1 friendly.
 
 # make riak exterinally accessible.
+iptables -t nat -A PREROUTING -p tcp -s 124.207.235.2 -d 211.154.172.172 --dport 58098 -j DNAT --to 192.168.88.1:58098
 iptables -A FORWARD -p tcp -s 124.207.235.2 -d 211.154.172.172 --dport 58098 -j ACCEPT
 iptables -A INPUT -p tcp -s 124.207.235.2 --dport 58098 -j ACCEPT
 
 # make the `fan-out part of "the router of crawler"` externally accessible.
+iptables -t nat -A PREROUTING -p tcp -s 124.207.235.2 -d 211.154.172.172 --dport 50011 -j DNAT --to 192.168.88.1:50011
 iptables -A FORWARD -p tcp -s 124.207.235.2 -d 211.154.172.172 --dport 50011 -j ACCEPT
 iptables -A INPUT -p tcp -s 124.207.235.2 --dport 50011 -j ACCEPT
 
