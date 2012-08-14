cb4f909d35d70d245feaa4c85370d78b551cb3be
frank <wumuch@gmail.com>
2012-08-14 18:03:11 +0800

    revision

--- toolkit/iptables.sh
+++ toolkit/iptables.sh
@@ -3,7 +3,6 @@
 ### delete the `drop all`
 
 # make gad1 friendly.
-iptables -A INPUT ! -s 124.207.235.2 -j DROP
 
 # make riak exterinally accessible.
 iptables -A FORWARD -p tcp -s 124.207.235.2 -d 211.154.172.172 --dport 58098 -j ACCEPT
@@ -12,4 +11,9 @@ iptables -A INPUT -p tcp -s 124.207.235.2 --dport 58098 -j ACCEPT
 # make the `fan-out part of "the router of crawler"` externally accessible.
 iptables -A FORWARD -p tcp -s 124.207.235.2 -d 211.154.172.172 --dport 50011 -j ACCEPT
 iptables -A INPUT -p tcp -s 124.207.235.2 --dport 50011 -j ACCEPT
+
+# sometimes...
+iptables -A INPUT ! -s 124.207.235.2 -j DROP
+iptables -A INPUT -j DROP
+iptables -A FORWARD -j DROP
 
