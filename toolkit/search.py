1e0e28f022f06dceb8834536027960ab2f77f8d6
frank <wumuch@gmail.com>
2012-08-14 13:53:42 +0800

    revision

--- toolkit/search.py
+++ toolkit/search.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #coding:utf-8
 
 import os, sys
@@ -6,16 +7,30 @@ from riak import RiakClient
 from config import config
 from pprint import pprint
 
-bucks = ('loc', 'mov', 'ads')
+bucks = {u'loc':u'words', u'mov':u'ref', u'ads':u'words'}
+default_buck = u'loc'
 def search(*args):
-    assert len(args) > 0, AssertionError('please specify at least one argument')
-    buck = args[0] if args[0] in bucks else 'loc'
-    term = ' '.join(map(lambda w: unicode(w, 'utf-8'), args[1:]))
-    return riak.search(buck, u'loc:' + term)
+    assert len(args) > 0, AssertionError('kid, please specify at least one argument')
+    buck = field = None
+    if len(args) > 1:
+        if '.' in args[0]:
+            buck, field = map(lambda w: unicode(w, 'utf-8'), args[0].split('.'))
+        else:
+            buck = unicode(args[0], 'utf-8')
+    buck_specified = buck in bucks
+    if buck_specified:
+        if field is None:
+            field = bucks[buck]
+    else:
+        buck, field = default_buck, bucks[default_buck]
+    term = field + u':' + u' '.join(map(lambda w: unicode(w, 'utf-8'), args[buck_specified:]))
+    print u'search ' + term + u' on ' + buck
+    return riak.search(buck, term)
 
 def export(var):
     if isinstance(var, basestring):
-        return pprint(var)
+        print(var)
+        return
     if isinstance(var, dict):
         for k, v in var.iteritems():
             print k, ':',
@@ -25,9 +40,10 @@ def export(var):
         for v in var:
             export(v)
         return
+    print var
 
 riak = RiakClient(**config.getRiak())
-query = search(*sys.argv)
+query = search(*sys.argv[1:])
 
 count = 0
 for record in query.run():
