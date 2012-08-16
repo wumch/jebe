8b38103973d969bd350df46cb793e34cac3b6010
frank <wumuch@gmail.com>
2012-08-16 12:28:43 +0800

    revision

--- target/service/model/matcher.py
+++ target/service/model/matcher.py
@@ -8,16 +8,13 @@ from utils.MarveWords import MarveWords
 class Matcher(RiakStorer):
 
     buckId = 'ads'
-    if DEBUG:
-        backId = 'asus'
-    buck = config.bucks[buckId]['buck']
     field = 'words'
     field = u'words'
 
     def __init__(self):
         super(Matcher, self).__init__()
 
-    def match(self, words=None, content=None, loc=None, buck=buck, field=field):
+    def match(self, words=None, content=None, loc=None, buck=config.bucks[buckId]['buck'], field=field):
         if words is None and content is None and loc is not None:
             splited_content = self._fetchSplitedContent(loc)
             ws = MarveWords(content=splited_content).top()
@@ -26,7 +23,7 @@ class Matcher(RiakStorer):
         return self.search(ws, buck=buck, field=field)
 
     def search(self, words, buck=buck, field=field):
-        return [record.get().get_data() for record in self._search(words=words, buck=buck, field=field).run()]
+        return [record.get().get_data() for record in self._search(words=words, buck=config.bucks[buckId]['buck'], field=field).run()]
 
     def _search(self, words, buck, field):
         term = field + u':' + u' '.join(map(lambda w: w if isinstance(w, unicode) else unicode(w, 'utf-8'), words))
