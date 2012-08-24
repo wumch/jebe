f77d9843be65e7b173ab1aaee526051363df40fe
frank <wumuch@gmail.com>
2012-08-24 18:05:52 +0800

    revision

--- target/service/tads/handler.py
+++ target/service/tads/handler.py
@@ -59,7 +59,7 @@ class HAdsByLoc(Handler):
         url = self.params['url']
         if self.pageExists(url):
             self.ads = [{'link':a['link'], 'text':a['text'], 'id':a['id']} for a in self.matcher.match(loc=url)]
-            if self.ads: logger.info('ad shown: %(text)s [%(link)s]' % self.ads[0])
+            if self.ads: logger.info(('ad shown: %(text)s [%(link)s] on [%%s]' % self.ads[0]) % url)
         else:
             self._replyContent(sysconfig.RPC_FUNC_NAME['crawlPage'] + '();')
 #
