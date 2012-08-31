d395331d7e1d77b052879e663b4a6b304db22c40
frank <wumuch@gmail.com>
2012-08-31 18:02:47 +0800

    qdb revision

--- qdb/config.cpp
+++ qdb/config.cpp
@@ -30,7 +30,11 @@ void Config::init(int argc, char* argv[])
 	;
 
 	boost::filesystem::path program_path = argv[0];
+#if BOOST_VERSION > 104200
 	program_name = program_path.filename().string();
+#else
+	program_name = program_path.filename();
+#endif
 
 	boost::program_options::variables_map cmdOptions;
 	try
