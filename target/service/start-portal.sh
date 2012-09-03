35964afcfdee18cc812e7e9ed5ce81fb2cdd4d9a
frank <wumuch@gmail.com>
2012-09-03 21:20:50 +0800

    tiny tidy

---
+++ target/service/start-portal.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ "$#" -gt "0" ]; then
+    ports="$*"
+else
+    ports=(10020 10021 10022)
+fi
+
+cd $(dirname $(realpath $0))
+
+start_portal_on () {
+    port="$1"
+    sed -i "/app.listen(port=/ s/\(port=\)[0-9]*/\1${port}/" portal.py
+    nohup python portal.py >/dev/null 2>&1 &
+}
+
+for i in ${ports[@]}; do
+    echo "will listen on port ${i}"
+    start_portal_on "${i}"
+done
+
