ddd4cdbff80d4d7a70af6318e202fa55af02b7bd
frank <wumuch@gmail.com>
2012-09-04 10:26:24 +0800

    revision

--- toolkit/ctr.sh
+++ toolkit/ctr.sh
@@ -1,7 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-shown=$(grep 'ad shown:' /var/log/target-server.log | grep -v 坦克世界 | wc -l)
-clicked=$(grep -v '?ad=30' /var/log/nginx/adclick | awk '{if ($9!="124.207.235.2"&&($9!=last["ip"]||$12!=last["ad"])){last["ip"]=$9;last["ad"]=$12;total+=1;}else{last["ip"]="ie6"}}END{print total}')
+#shown=$(grep 'ad shown:' /var/log/target-server.log | grep -v 坦克世界 | wc -l)
+cmd="grep 'ad shown:' /var/log/adsys.log | wc -l"
+declare -i shown=0
+shown+=$(ssh 192.168.88.2 "${cmd}")
+shown+=$(ssh 192.168.88.3 "${cmd}")
+shown+=$(ssh 192.168.88.4 "${cmd}")
+clicked=$(awk '{if ($9!="124.207.235.2"&&($9!=last["ip"]||$12!=last["ad"])){last["ip"]=$9;last["ad"]=$12;total+=1;}else{last["ip"]="ie6"}}END{print total}' /var/log/nginx/adclick)
 echo "shown: ${shown}, clicked: ${clicked}"
 printf "ctr: 0.%04d%%\n" $((clicked*1000000/shown))
 
