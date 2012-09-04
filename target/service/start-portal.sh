6a830785c8ae79d2a6b47b7b23f476c01aa52f0d
frank <wumuch@gmail.com>
2012-09-04 13:09:45 +0800

    revision

--- target/service/start-portal.sh
+++ target/service/start-portal.sh
@@ -3,7 +3,7 @@
 if [ "$#" -gt "0" ]; then
     ports="$*"
 else
-    ports=(10020 10021 10022 10023 10024 10025)
+    ports=(10020 10021 10022 10023 10024 10025 10026 10027 10028 10029 10030)
 fi
 
 kill `ps aux|grep 'python portal.py'|grep -v grep|awk '{print $2}'|xargs`
