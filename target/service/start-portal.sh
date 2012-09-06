a8622504c7099da438b3f9d58d3a58a52b0cdd2a
frank <wumuch@gmail.com>
2012-09-06 12:43:04 +0800

    revision

--- target/service/start-portal.sh
+++ target/service/start-portal.sh
@@ -6,10 +6,16 @@ else
     ports=(10020 10021 10022 10023 10024 10025 10026 10027 10028 10029 10030)
 fi
 
-kill `ps aux|grep 'python portal.py'|grep -v grep|awk '{print $2}'|xargs`
+kill `ps aux|grep '\(python portal.py\|python dealer.py\)'|grep -v grep|awk '{print $2}'|xargs`
 sleep 1
 
 cd $(dirname $(realpath $0))
+
+for ((i=0;i<3;++i)); do
+    echo "starting dealer ${i}"
+    nohup python dealer.py >/dev/null 2>&1 &
+    sleep 0.2
+done
 
 start_portal_on () {
     port="$1"
