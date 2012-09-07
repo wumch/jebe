1250227a0b763518be6a8f36384a2c320cf335b1
frank <wumuch@gmail.com>
2012-09-07 15:45:32 +0800

    cws.sh

--- cws/cws.sh
+++ cws/cws.sh
@@ -1,44 +1,54 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #
 # rc-script for service `tcmd`
 #
 
 PROGRAM_NAME="cws"
+TCMD_EXEC="bin/${PROGRAM_NAME}"
 USER="$(whoami)"
 OUTLOG=/var/log/${PROGRAM_NAME}.output.log
 ERRLOG=/var/log/${PROGRAM_NAME}.error.log
-pidfile=/var/run/${PROGRAM_NAME}.pid
+#pidfile=/var/run/${PROGRAM_NAME}.pid
 
 ROOT_PATH="$(dirname $(realpath ${0}))"
 ROOT_PATH=${ROOT_PATH%/bin}
 if [ ! -n "${ROOT_PATH}" ]; then
-    echo -e "\033[32;31;5mcan not resolve home directory for ${PROGRAM_NAME}! maybe you do not have 'realpath'.\033[0m"
+    echo -e "\033[32;31;5mcan not resolve home directory for tokenizer! maybe you do not have 'realpath'.\033[0m"
     exit 2
 fi
 
 cd ${ROOT_PATH}
-TCMD_EXEC="bin/${PROGRAM_NAME}"
+execfile=$(echo "${TCMD_EXEC}")
+execfile="${TCMD_EXEC%% *}"
+if [ ! -n "${execfile}" ]; then
+    echo "execute-able file is null"
+    exit 1
+elif [ "${execfile}" == "${TCMD_EXEC}" ] && [ ! -x "${execfile}" ] && [ ! -n "$(which ${execfile})" ]; then
+    echo "the ${exexfile} is not a execute-able"
+    exit 1
+fi
 
-if [ -n "${pidfile}" ]; then
-    pidfile=$(awk -F'\\s*=\\s*' '/pid-file *\=/ {pidf=$2; sub(/^[^\/]+/, "", pidf); sub(/[^\w\d]+$/, "", pidf); print pidf;}' "${ROOT_PATH}/etc/${PROGRAM_NAME}.conf")
+if [ ! -n "${pidfile}" ]; then
+    pidfile=$(awk -F'\\s*=\\s*' '/pid-?file *\=/ {pidf=$2; sub(/^[^\/]+/, "", pidf); sub(/[^\w\d]+$/, "", pidf); print pidf;}' "${ROOT_PATH}/etc/${PROGRAM_NAME}.conf")
 fi
 
 if [ ! -n "${pidfile}" ]; then
     echo -e "\033[32;31;5mcannot resolve pidfile for ${PROGRAM_NAME}!\033[0m"
     exit 1
+elif [ ! -d "$(dirname ${pidfile})" ]; then
+    echo -e "\033[32;31;5mpath of pidfile [$(dirname ${pidfile})] for ${PROGRAM_NAME} non-exists!\033[0m"
+    exit 1
 fi
 
 zstart() {
     if [ -n "$(zgetpid)" ];then
-        printf "\033[32;31;5${PROGRAM_NAME} malready started!\033[0m\n"
+        printf "\033[32;31;5m${PROGRAM_NAME} already started!\033[0m\n"
         return
     fi
-    local res=$(zstartup)
+    zstartup
     if [ "$?" -eq "0" ]; then
         printf "\033[32;49;5m${PROGRAM_NAME} started!\033[0m\n"
-    else
-        printf "\033[32;31;5mstart ${PROGRAM_NAME} faild!\033[0m\nerror:\n%s\n" "${res}"
     fi
 }
 
@@ -77,7 +87,7 @@ zgetpid () {
     if [ -r "${pidfile}" ]; then
         local pid=$(cat "${pidfile}")
         if [ -n "${pid}" ]; then
-            if [ -d "/proc/${pid}" ]; then
+            if [ -n "$(zpidalive ${pid})" ]; then
                 echo "${pid}"
             else
                 >"${pidfile}"
@@ -99,13 +109,34 @@ zsuexec () {
     if [ ! -n "${cmd}" ]; then
         return
     fi
+    local lastbpid="$!"
     if [ -n "${USER}" -a "${USER}" != "$(whoami)" ]; then
         su "${USER}" -c "nohup ${cmd} >>"${OUTLOG}" 2>>"${ERRLOG}" &"
+        local command="su ${USER} -c nohup ${cmd} >>${OUTLOG} 2>>${ERRLOG} &"
     else
         nohup ${cmd} >>"${OUTLOG}" 2>>"${ERRLOG}" &
+        local command="nohup ${cmd} >>${OUTLOG} 2>>${ERRLOG} &"
     fi
-    echo ${res}
-    return $?
+    local status="$?"
+    sleep 0.3
+    if [ ! -n "$(zgetpid)" ] && [ -n "$!" ] && [ "x$!" != "x${lastbpid}" ] && [ -n "${pidfile}" ]; then
+        if [ -n "$(zpidalive $!)" ]; then
+            echo "$!" > "${pidfile}"
+        else
+            echo -e "\033[32;31;5mfailed on start ${PROGRAM_NAME}\033[0m\ncommand:\n${command}"
+            return 1
+        fi
+    fi
+    return ${status}
+}
+
+zpidalive () {
+    local pid="$1"
+    if [ ! -n "${pid}" ] || [ ! -d "/proc/${pid}" ]; then
+        echo ""
+    else
+        echo "1"
+    fi
 }
 
 usage () {
@@ -115,6 +146,7 @@ usage () {
 
 
 if [ "$#" -eq "0" ]; then
+    usage
     act="status"
 else
     act="$1"
