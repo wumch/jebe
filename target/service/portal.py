b65d42cecce5857fb2288da66cffc8635a81a76d
frank <wumuch@gmail.com>
2012-09-12 20:40:52 +0800

    revision

--- target/service/portal.py
+++ target/service/portal.py
@@ -13,6 +13,15 @@ except ImportError:
 from utils.natip import natip
 from controler.wsgiwrap import WsgiApp
 
+def run():
+    addr = (natip, 8001)
+    backlog = 512
+    app = WsgiApp()
+    if gevent.version_info < (0, 13, 8):
+        HTTPServer(listener=addr, handle=app, backlog=backlog).serve_forever()
+    else:
+        HTTPServer(app).serve_forever(socket_or_address=addr, backlog=512)
+
 if __name__ == '__main__':
-    greenlets = [gevent.spawn(HTTPServer((natip, 8001), handle=WsgiApp()).serve_forever)]
+    greenlets = [gevent.spawn(run)]
     gevent.joinall(greenlets=greenlets)
