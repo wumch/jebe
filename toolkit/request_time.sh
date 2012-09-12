fc172202edf6f1e4867a3085e307d5351f9e00fa
frank <wumuch@gmail.com>
2012-09-12 21:01:58 +0800

    revision

--- toolkit/request_time.sh
+++ toolkit/request_time.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-tail -n 10000 /dev/shm/nginx_target.log | awk '{t=($7<1)?(int($7*100)/100):int($7/5+1)*5; arr[t]+=1}END{for (i in arr){print i"s\t"arr[i]}}' | sort -n -k 1
+tail -n ${1:-10000} /dev/shm/nginx_target.log | awk '{t=($7<1)?(int($7*100)/100):int($7/5+1)*5; arr[t]+=1}END{for (i in arr){print i"s\t"arr[i]}}' | sort -n -k 1
 
