1a55ecfa4debe79fc61004357af8251ae0dadb40
frank <wumuch@gmail.com>
2012-09-12 21:21:13 +0800

    revision

--- toolkit/status.sh
+++ toolkit/status.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tail -n 10000 /dev/shm/nginx_target.log | awk -F'"' '{status[$2]+=1;if($3==19){crawlOrEmpty+=1}else{shown+=1}}END{print "shown: "shown"\ncrawl-or-empty-ads: "crawlOrEmpty"\n"; for (i in status){print "response-stats["i"]: "status[i]}}'
+tail -n ${1:-10000} /dev/shm/nginx_target.log | awk -F'"' '{status[$2]+=1;if($3==19){crawlOrEmpty+=1}else{shown+=1}}END{print "shown: "shown"\ncrawl-or-empty-ads: "crawlOrEmpty"\n"; for (i in status){print "response-stats["i"]: "status[i]}}'
 
