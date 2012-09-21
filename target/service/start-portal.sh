566765adadbc031999ce5c6a9436c22192b3790e
frank <wumuch@gmail.com>
2012-09-21 14:20:22 +0800

    start-portal.sh

--- target/service/start-portal.sh
+++ target/service/start-portal.sh
@@ -6,15 +6,21 @@ else
     ports=(10030 10031 10032 10033)
 fi
 
-pids=`ps aux|grep 'python[0-9\.]* \(portal.py\|python dealer.py\)'|grep -v grep|awk '{print $2}'|xargs`
-if [ -n "${pids}" ]; then
-    kill ${pids}
-fi
-sleep 0.3
+#pids=`ps aux|grep 'python[0-9\.]* \(portal.py\|python dealer.py\)'|grep -v grep|awk '{print $2}'|xargs`
+#if [ -n "${pids}" ]; then
+#    kill ${pids}
+#fi
+#sleep 0.3
 
 cd $(dirname $(realpath $0))
 
-restart_dealer () {
+restart_dealer() {
+    local pids=`ps aux|grep "python[0-9\.]* *dealer.py"|grep -v grep|awk '{print $2}'|xargs`
+    if [ -n "${pids}" ]; then
+        echo "kill dealers: ${pids}"
+        kill ${pids}
+        sleep 0.1 
+    fi  
     for ((i=0;i<3;++i)); do
         echo "starting dealer ${i}"
         nohup python dealer.py >/dev/null 2>&1 &
@@ -22,12 +28,18 @@ restart_dealer () {
     done
 }
 
-start_portal_on () {
-    port="$1"
-    #sed -i "/addr \= (natip/ s/\(natip\,\s*\)[0-9]*/\1${port}/" portal.py
+start_portal_on() {
+    local port="$1"
+    local pid=`ps aux|grep "python[0-9\.]* *\(portal.py\|dealer.py\) *${port}"|grep -v grep|awk '{print $2}'|xargs`
+    if [ -n "${pid}" ]; then
+        echo "kill portal: ${pid}"
+        kill ${pid}
+        sleep 0.1
+    fi
     nohup python portal.py "${port}" >/dev/null 2>&1 &
 }
 
+restart_dealer
 for i in ${ports[@]}; do
     echo "will listen on port ${i}"
     start_portal_on "${i}"
