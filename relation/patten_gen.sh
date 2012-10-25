96444417e64e6c22a485955ae7bf8b88aad78485
frank <wumuch@gmail.com>
2012-10-25 17:49:59 +0800

    revision

--- relation/patten_gen.sh
+++ relation/patten_gen.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cd $(dirname $(realpath $0))
+ROOT_PATH=$(dirname $(realpath $0))
+ROOT_PATH=${ROOT_PATH%/bin}
+cd "${ROOT_PATH}"
 
-wordfile="{$1-../cws/etc/words.txt}"
-outfile="${2-etc/patten.txt}"
+wordfile="${1:-../cws/etc/words.txt}"
+outfile="${2:-etc/patten.txt}"
 
 awk '{if (NR>1){print $1}}' ${wordfile} > ${outfile}
 
