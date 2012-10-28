89d21df89ee91650789d0471a14dfbd3bbd412af
frank <wumuch@gmail.com>
2012-10-29 01:03:11 +0800

    revision

--- relation/calculater.cpp
+++ relation/calculater.cpp
@@ -87,7 +87,7 @@ void Calculater::ready()
 	std::sort(worddf.begin(), worddf.end());
 	maxdf = worddf[static_cast<wordid_t>(Aside::config->df_quantile_top * worddf.size())];
 	mindf = worddf[static_cast<wordid_t>(Aside::config->df_quantile_bottom * worddf.size())];
-	LOG(INFO) << "total-documents: " << Aside::totalDocNum << ", maxdf: " << maxdf << ", mindf: " << mindf;
+	CHECK(Aside::config->loglevel > 0) << "total-documents: " << Aside::totalDocNum << ", maxdf: " << maxdf << ", mindf: " << mindf;
 }
 
 void Calculater::filter()
@@ -97,11 +97,11 @@ void Calculater::filter()
 		if (!shouldSkip(wdlist[wordid]))
 		{
 			toProper(wdlist[wordid], wpmap.insert(std::make_pair(wordid, VaredProperList())).first->second);
-			LOG(INFO) << "reserved [" << Aside::wordmap[wordid] << "](" << wdlist[wordid].size() << "),(" << wpmap[wordid].ex << "," << wpmap[wordid].var_sqrt << ")";
+			CHECK(Aside::config->loglevel > 1) << "reserved [" << Aside::wordmap[wordid] << "](" << wdlist[wordid].size() << "),(" << wpmap[wordid].ex << "," << wpmap[wordid].var_sqrt << ")";
 		}
 		else if (!wdlist[wordid].empty())
 		{
-			LOG(INFO) << "skipped [" << Aside::wordmap[wordid] << "](" << wdlist[wordid].size() << ")";
+			CHECK(Aside::config->loglevel > 1) << "skipped [" << Aside::wordmap[wordid] << "](" << wdlist[wordid].size() << ")";
 		}
 		wdlist[wordid].clear();
 	}
@@ -120,7 +120,7 @@ void Calculater::calcu()
 			if (CS_BLIKELY(iter != it))
 			{
 				c = corr(plist_1, iter->second);
-				LOG(INFO) << "cov(" << Aside::wordmap.getWordById(it->first) << "," << Aside::wordmap.getWordById(iter->first) << ") = " << c;
+				CHECK(Aside::config->loglevel > 2) << "corr(" << Aside::wordmap.getWordById(it->first) << "," << Aside::wordmap.getWordById(iter->first) << ") = " << c;
 				if (c >= Aside::config->min_corr)
 				{
 					simlist.push_back(Similarity(iter->first, c));
