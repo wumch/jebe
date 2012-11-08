ebd4b36ac507d9654e5093986cbd197f1c29467b
frank <wumuch@gmail.com>
2012-11-08 16:02:09 +0800

    revision

--- target/service/controler/wsgi_crawler.py
+++ target/service/controler/wsgi_crawler.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 
+import os
 import zlib
 from config import config, logger
 from wsgi_controler import WsgiControler
@@ -9,7 +10,11 @@ class WsgiCrawler(WsgiControler):
 
     def __init__(self, environ, start_response):
         super(WsgiCrawler, self).__init__(environ, start_response)
-        self.postData = self.env.get('wsgi.input').read() if 'wsgi.input' in self.env else None
+        if 'CONTENT_LENGTH' in self.env and 'wsgi.input' in self.env:
+            self.postData = self.env.get('wsgi.input').read(int(self.env['CONTENT_LENGTH']))
+        else:
+            logger.error("environ: " + str(self.env))
+            self.postData = None
         self.pageStorer = PageStorer.instance()
 
     def handle(self):
@@ -19,6 +24,7 @@ class WsgiCrawler(WsgiControler):
         if not self.postData:
             return False
         if self.postData[-1] != 'V' or self.postData[0] != 'A':
+            logger.error("<%s(%d/%d)>[%s][%s]" % (type(self.postData), len(self.postData), int(self.env['CONTENT_LENGTH']), self.postData[:5], self.postData[-5:]))
             return 405
         try:
             info = config.jsoner.decode(zlib.decompress(self.postData[2:-1]))
