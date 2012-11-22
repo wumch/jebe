a7bb511c86dd9b84468e377969f0c44d0d47aa0d
frank <wumuch@gmail.com>
2012-11-22 11:51:13 +0800

    mongodb::paths regress script

--- toolkit/mongo_regress_paths.py
+++ toolkit/mongo_regress_paths.py
@@ -7,10 +7,9 @@ if src_path not in sys.path:
 import time
 import pymongo
 from config import config, logger
-from driversync.tokenizer import Tokenizer
 from utils.urlparser import UrlParser
 
-class PageStorer(object):
+class Regressor(object):
 
     _instance = None
     urlParser = UrlParser()
@@ -22,15 +21,11 @@ class PageStorer(object):
         return cls._instance
 
     def __init__(self):
-        self.maxts = 1352304000     # 2012-11-08 00:00:00
-        self.mints = self.maxts - (86400 * 7)    # one week ago
-        self.curts = 1352736000     # 2012-11-13 00:00:00
         self.curfinished = 0
         self.nextStop = 10000
         self.prepare()
 
     def prepare(self):
-        self.tokenizer = Tokenizer.instance()
         self.connections = {}
         self.dbs = {}
         self.collections = {}
@@ -74,10 +69,7 @@ class PageStorer(object):
         }
         self.collections['paths'].insert(paths)
 
-    def _marve(self, content):
-        return self.tokenizer.marve(content=content)
-
 if __name__ == '__main__':
     nextSkip = int(sys.argv[1]) if len(sys.argv) > 1 else 0
     while nextSkip is not None:
-        nextSkip = PageStorer().run(skip=nextSkip)
+        nextSkip = Regressor().run(skip=nextSkip)
