8c0ab8f723ad5c3e6308012379a871e5f9fef519
frank <wumuch@gmail.com>
2012-11-28 19:33:32 +0800

    revision

--- cluster/kmeans/vector.hpp
+++ cluster/kmeans/vector.hpp
@@ -44,35 +44,76 @@ public:
 	RawVector rv;
 
 public:
-	Vector();
+	Vector()
+		: rv(Aside::totalFeatureNum, Aside::config->reserve_fnum)
+	{}
 
 	/*
 	 * store only the result of GuiYiHua.
 	 * GuiYiHua(vector) = vector / mod(vec).
 	 */
-	explicit Vector(const Document& doc);
+	explicit Vector(const Document& doc)
+		: VectorMeta(doc.id), rv(Aside::totalFeatureNum, doc.flist.size())
+	{
+		init_from_iv(doc.flist);
+	}
 
-	explicit Vector(vid_t id_, const RawVector& rv_);
+	explicit Vector(vid_t id_, const RawVector& rv_)
+		: VectorMeta(id_), rv(rv_)
+	{}
 
 	// fix the copy-constructor of boost::mapped_vector.
-	Vector(const Vector& vec);
+	Vector(const Vector& vec)
+		: VectorMeta(vec.id, vec.belong_cls), rv(vec.rv.size(), vec.rv.nnz())
+	{
+		rv.assign(vec.rv);
+	}
 
-	void reset(const RawVector& rv_);
+	void reset(const RawVector& rv_)
+	{
+		rv = rv_;
+	}
 
 public:
-	CS_FORCE_INLINE RawVector* operator->();
-	CS_FORCE_INLINE const RawVector* operator->() const;
+	CS_FORCE_INLINE RawVector* operator->()
+	{
+		return rv;
+	}
+	CS_FORCE_INLINE const RawVector* operator->() const
+	{
+		return rv;
+	}
 
-	CS_FORCE_INLINE RawVector& operator*();
-	CS_FORCE_INLINE const RawVector& operator*() const;
+	CS_FORCE_INLINE RawVector& operator*()
+	{
+		return &rv;
+	}
+	CS_FORCE_INLINE const RawVector& operator*() const
+	{
+		return &rv;
+	}
 
-	CS_FORCE_INLINE operator RawVector();
+	CS_FORCE_INLINE operator RawVector()
+	{
+		return rv;
+	}
+
 
 private:
-	void init_from_iv(const InputVector& vec);
+	void init_from_iv(const InputVector& vec)
+	{
+		copy_from_iv(vec);
+	}
 
 private:
-	void copy_from_iv(const InputVector& vec);
+	void copy_from_iv(const InputVector& vec)
+	{
+		decimal_t mod_reci = 1 / mod(vec);		// no need of checking for division by zero.
+		for (InputVector::const_iterator it = vec.begin(); it != vec.end(); ++it)
+		{
+			rv.insert_element(it->fid, mod_reci * it->fval);
+		}
+	}
 };
 
 class VecFactory
