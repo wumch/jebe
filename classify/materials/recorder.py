2773d87272090852fdc0a598702498974e736a06
frank <wumuch@gmail.com>
2012-12-03 18:28:22 +0800

    revision

--- classify/materials/recorder.py
+++ classify/materials/recorder.py
@@ -1,12 +1,17 @@
 #coding:utf-8
 
+import os
 from materials import cates
 
 class Recorder(object):
 
     def __init__(self):
         self.counter = [0 for i in range(0, max(cates.keys()) + 2)]
+        self.outfile = r'/hdd1/classify/materials.txt'
+#        self.outfile = r'/tmp/materials.txt'
+        self.fp = open(self.outfile, 'w')
+        self.format = "%d\t%s" + os.linesep
 
     def record(self, doc, cate):
         self.counter[cate] += 1
-        print "%d\t%s" % (cate, doc['url'])
+        self.fp.write(self.format % (cate, doc['url']))
