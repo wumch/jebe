07a58392787af83cf4a9a97052ac6785719001a5
frank <wumuch@gmail.com>
2013-02-12 01:00:00 +0800

    revision

--- swatch/collect.sh
+++ swatch/collect.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+declare -a servers=(192.168.88.1 192.168.88.3 192.168.88.6)
 logfile=${1:-/dev/shm/crawler.log}
 hostname="$(hostname -i)"
-for line in $(grep -Ev '^\s*#' /code/cluster-control/servers.list)
+for line in "${servers[@]}";
 do
 	if [ "${line}" != "${hostname%% *}" ]; then
         nohup ssh ${line} "tail -f /var/log/nginx/ad.i8ad.cn.access.log" >> ${logfile} 2>/dev/null &
